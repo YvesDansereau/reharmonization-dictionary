f979578f82df9a1534ae087694e383a6601db048
Yuichiro DAN <yuichirodan1022@gmail.com>
2023-08-24 15:48:35 +0900

    Top: add a navigation to detail page

--- components/Top.tsx
+++ components/Top.tsx
@@ -1,12 +1,24 @@
 import { Headline, List } from 'react-native-paper';
+import type { FC } from 'react'
+import { Pressable } from 'react-native';
 
-export const Top = () => {
+type TopProps = {
+    navigation: any //TODO: specify the class
+};
+
+export const Top: FC<TopProps> = props => {
+    const handlePress = (name: string): void => {
+        props.navigation.navigate(name);
+    };
+
     return (
         <>
             <Headline>リハモ辞典</Headline>
             <List.AccordionGroup>
                 <List.Accordion title="セブンスコードのリハモ" id="1">
-                    <List.Item title="ツーファイブへの分割" />
+                    <Pressable onPress={() => {handlePress("DetailSeventhToTwoFive")}}>
+                        <List.Item title="ツーファイブへの分割" />
+                    </Pressable>
                 </List.Accordion>
             </List.AccordionGroup>
         </>
