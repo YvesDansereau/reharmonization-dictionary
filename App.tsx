c68af45689bf75530535f5cc07360655d55709af
Yves Dansereau <54108421+YvesDansereau@users.noreply.github.com>
2023-08-24 14:46:18 +0900

    ✨ Add top page prototype (#3)
    * App: add PaperProvider and SafeAreaView
    
    * Top: add top page
    
    * App: install top page

--- App.tsx
+++ App.tsx
@@ -1,20 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { PaperProvider, Text } from 'react-native-paper';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Top } from './components/Top';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <PaperProvider>
+      <SafeAreaView>
+        <Top />
+      </SafeAreaView>
       <StatusBar style="auto" />
-    </View>
+    </PaperProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
