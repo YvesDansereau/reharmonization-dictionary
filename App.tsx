4c5efb6ec4d27bb443f02ab50c6334681d25d781
Yuichiro DAN <yuichirodan1022@gmail.com>
2023-08-24 15:47:10 +0900

    App: install navigation

--- App.tsx
+++ App.tsx
@@ -2,14 +2,22 @@ import { StatusBar } from 'expo-status-bar';
 import { PaperProvider, Text } from 'react-native-paper';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Top } from './components/Top';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { DetailSeventhToTwoFive } from './components/Detail';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <PaperProvider>
-      <SafeAreaView>
-        <Top />
-      </SafeAreaView>
-      <StatusBar style="auto" />
-    </PaperProvider>
+    <NavigationContainer>
+      <PaperProvider>
+          <Stack.Navigator initialRouteName='Top'>
+            <Stack.Screen name='Top' component={Top}/>
+            <Stack.Screen name='DetailSeventhToTwoFive' component={DetailSeventhToTwoFive} options={{title: "ツーファイブへの分割"}}/>
+          </Stack.Navigator>
+        <StatusBar style="auto" />
+      </PaperProvider>
+    </NavigationContainer>
   );
 }
