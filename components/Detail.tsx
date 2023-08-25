42be219aa4eb2160bb4f8d970e732bf3dbad1343
Yuichiro DAN <yuichirodan1022@gmail.com>
2023-08-25 10:26:36 +0900

    Detail: add contents

--- components/Detail.tsx
+++ components/Detail.tsx
@@ -1,20 +1,38 @@
 import type { FC } from 'react';
-import { Headline } from 'react-native-paper';
+import { Text } from 'react-native-paper';
 
 type DetailBaseProps = {
-    headline: string;
+    what: string;
+    when: string;
+    how: string;
+    example: string
 };
 
 const DetailBase: FC<DetailBaseProps> = props => {
     return (
         <>
-            <Headline>{props.headline}</Headline>
+            <Text variant='titleLarge'>どんなもの？</Text>
+            <Text variant='bodyLarge'>{props.what}</Text>
+
+            <Text variant='titleLarge'>主にいつ使える？</Text>
+            <Text variant='bodyLarge'>{props.when}</Text>
+
+            <Text variant='titleLarge'>どうやって使う？</Text>
+            <Text variant='bodyLarge'>{props.how}</Text>
+
+            <Text variant='titleLarge'>例</Text>
+            <Text variant='bodyLarge'>{props.example}</Text>
         </>
     );
 };
 
 export const DetailSeventhToTwoFive: FC = () => {
     return (
-        <DetailBase headline='test'/>
+        <DetailBase 
+            what='セブンスコードをツーファイブに分割できる' 
+            when='セブンスコードがドミナントまたはセカンダリードミナントとして四度上のコードに解決するとき' 
+            how='セブンスコードが指定されている部分の前半分をを四度下のマイナーセブンスコードまたはハーフディミニッシュコードに変える' 
+            example='|G7    |CM7    | --> |Dm7 G7 |CM7    |'
+        />
     );
 };
