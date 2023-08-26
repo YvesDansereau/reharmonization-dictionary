52ef50e115919d15e5617139972cae38433d0354
Yuichiro DAN <yuichirodan1022@gmail.com>
2023-08-26 21:55:41 +0900

    App: install type check for navigation

--- App.tsx
+++ App.tsx
@@ -1,22 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { PaperProvider, Text } from 'react-native-paper';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { PaperProvider } from 'react-native-paper';
 import { Top } from './components/Top';
-import { NavigationContainer, useNavigation } from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { DetailSeventhToTwoFive } from './components/Detail';
-import { PageName } from './types/PageName';
+import { Detail } from './components/Detail';
+import { NavigationStackParamList } from './types/NavigationStackParamList';
 
-const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator<NavigationStackParamList>();
 
 export default function App() {
   return (
     <NavigationContainer>
       <PaperProvider>
-          <Stack.Navigator initialRouteName='Top'>
-            <Stack.Screen name={PageName.Top} component={Top}/>
-            <Stack.Screen name={PageName.SeventhToTwoFive} component={DetailSeventhToTwoFive} />
-          </Stack.Navigator>
+        <Stack.Navigator initialRouteName='Top'>
+          <Stack.Screen name='Top' component={Top} />
+          <Stack.Screen name='Detail' component={Detail} />
+        </Stack.Navigator>
         <StatusBar style="auto" />
       </PaperProvider>
     </NavigationContainer>
