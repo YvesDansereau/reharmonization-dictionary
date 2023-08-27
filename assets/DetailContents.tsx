45ca6d5f4f5071b2a085caed8c554c66bb07bcb7
Yuichiro DAN <yuichirodan1022@gmail.com>
2023-08-27 14:09:17 +0900

    DetailContents: add new example

--- assets/DetailContents.tsx
+++ assets/DetailContents.tsx
@@ -5,5 +5,8 @@ export const DetailContentSeventhToTwoFive: DetailContent = {
     what: "セブンスコードをツーファイブに分割できる",
     when: "セブンスコードがドミナントまたはセカンダリードミナントとして四度上のコードに解決するとき",
     how: "セブンスコードが指定されている部分の前半分をを四度下のマイナーセブンスコードまたはハーフディミニッシュコードに変える",
-    examples: "|G7    |CM7    | --> |Dm7 G7 |CM7    |"
+    examples: [
+        "|G7   |CM7   | --> |Dm7 G7 |CM7   |",
+        "|E7   |Am7   | --> |Bm7b5 E7 |Am7   |"
+    ]
 };
